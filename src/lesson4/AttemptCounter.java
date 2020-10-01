12110f83950e8718f60527c53f5d9c8f4755acf4
gralbert <albertgralin@gmail.com>
2020-10-01 10:55:29 +0700

    Make class AttemptCounter as Singleton.

--- src/lesson4/AttemptCounter.java
+++ src/lesson4/AttemptCounter.java
@@ -1,4 +1,14 @@
 package lesson4;
 
 public class AttemptCounter {
+    private static AttemptCounter instance;
+
+    // private AttemptCounter(){}
+
+    public static synchronized AttemptCounter getInstance(){
+        if (instance == null){
+            instance = new AttemptCounter();
+        }
+        return instance;
+    }
 }
